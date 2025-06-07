c0d8ba797a887b54fb399b9d78ecd10cdbd96d64
Addison <addisondouglas850@gmail.com>
2025-06-06 17:37:19 -0700

    Stop tracking ignored files

--- search_Engine/target/debug/deps/search_Engine-9e9b6e195d66d1b4.d
+++
@@ -1,5 +0,0 @@
-/home/broke/Coding_Projects/Browser/search_Engine/target/debug/deps/search_Engine-9e9b6e195d66d1b4: src/main.rs
-
-/home/broke/Coding_Projects/Browser/search_Engine/target/debug/deps/search_Engine-9e9b6e195d66d1b4.d: src/main.rs
-
-src/main.rs:
--- search_Engine/target/debug/search_Engine.d
+++
@@ -1,1 +0,0 @@
-/home/broke/Coding_Projects/Browser/search_Engine/target/debug/search_Engine: /home/broke/Coding_Projects/Browser/search_Engine/src/main.rs
